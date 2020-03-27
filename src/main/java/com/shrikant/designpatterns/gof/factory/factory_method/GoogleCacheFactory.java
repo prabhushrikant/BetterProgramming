f6a42f0aed0fdd1230b095ed67c4c8dd1728d896
shrikant prabhu <shrikant.prabhu@motorolasolutions.com>
2020-03-27 03:54:21 -0700

    Minor fix.
    Signed-off-by: shrikant prabhu <shrikant.prabhu@motorolasolutions.com>

--- src/main/java/com/shrikant/designpatterns/gof/factory/factory_method/GoogleCacheFactory.java
+++ src/main/java/com/shrikant/designpatterns/gof/factory/factory_method/GoogleCacheFactory.java
@@ -4,9 +4,8 @@ package com.shrikant.designpatterns.gof.factory.factory_method;
 public class GoogleCacheFactory<K, V> extends MemoryCacheFactory<K, V> {
 
   @Override
-  public GoogleCache<K, V> createCache(String cacheName) {
+  public MemoryCache<K, V> createCache(String cacheName) {
     return new GoogleCache<>(cacheName);
   }
-
-
+  
 }
