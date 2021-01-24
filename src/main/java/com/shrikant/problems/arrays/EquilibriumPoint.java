46805edb117625ba513a4f0ed8cd568bfca5dc90
Shrikant Prabhu <prabhushrikant@gmail.com>
2021-01-23 19:20:10 -0800

    added equilibrium point algo.

---
+++ src/main/java/com/shrikant/problems/arrays/EquilibriumPoint.java
@@ -0,0 +1,30 @@
+package com.shrikant.problems.arrays;
+
+// Equilibrium index of an array is an index such that the sum of elements at lower indexes is equal to the sum of elements at higher indexes. For example, in an array A:
+public class EquilibriumPoint {
+    
+    public int equilibriumPoint(int arr[], int n) 
+    {
+        // Your code here
+        if (n == 1) return 0;  //edge cases.
+
+        if (n%2 == 0) return -1; //edge cases.
+
+        long totalSum = 0;
+        for(long i : arr) {
+            totalSum += i;
+        }
+        
+        long leftSum = 0;
+        long rightSum = totalSum;
+        for (int i=0; i < n; i++) {
+            rightSum = rightSum - arr[i];
+            if (leftSum == rightSum) {
+                return i; //equilibrium point.
+            }            
+            leftSum += arr[i];
+        }
+        
+        return -1; //no such point exits.
+    }
+}
---
+++ src/test/java/com/shrikant/problems/arrays/EquilibriumPointTest.java
@@ -0,0 +1,48 @@
+package com.shrikant.problems.arrays;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
+public class EquilibriumPointTest {
+
+    EquilibriumPoint testObj;
+
+    private int[] inputArray;
+    private int expectedResult;
+
+    // constructor
+    public EquilibriumPointTest(int[] arr, int equilibriumPointIndex) {
+        this.inputArray = arr;
+        this.expectedResult = equilibriumPointIndex;
+    }
+
+    @Before
+    public void init() {
+        testObj = new EquilibriumPoint();
+    }
+
+    @Parameterized.Parameters
+    public static List<Object[]> data() {
+      return Arrays.asList(new Object[][] {
+         { new int[] {1}, 0 },
+         { new int[] {1, 3, 5, 2, 2}, 2 },
+      });
+   }
+
+
+
+    @Test
+    public void testEquilibriumPoint() {
+        assertThat(expectedResult, is(testObj.equilibriumPoint(inputArray, inputArray.length)));
+    }
+}
