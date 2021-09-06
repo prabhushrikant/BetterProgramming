c9119606030dae7bbdf72af9b37bf62447a21d6c
Shrikant Prabhu <prabhushrikant@gmail.com>
2021-09-05 21:38:05 -0700

    complexity explaination.

--- src/main/java/com/shrikant/problems/heaps/KthLargestItem.java
+++ src/main/java/com/shrikant/problems/heaps/KthLargestItem.java
@@ -3,6 +3,14 @@ package com.shrikant.problems.heaps;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
+//complexity explaination:
+// 1) Build a Max-Heap MH of the first k elements (arr[0] to arr[k-1]) of the given array. O(k)
+// 2) For each element, after the k’th element (arr[k] to arr[n-1]), compare it with root of MH. 
+// ……a) If the element is less than the root then make it root and call heapify for MH 
+// ……b) Else ignore it. 
+// // The step 2 is O((n-k)*logk)
+// 3) Finally, the root of the MH is the kth smallest element.
+// Time complexity of this solution is O(k + (n-k)*Logk)
 public class KthLargestItem {
 
     public int getKthLargestItem(int[] arr, int k)
