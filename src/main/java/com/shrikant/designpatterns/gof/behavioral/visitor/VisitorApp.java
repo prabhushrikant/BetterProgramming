e1337f4edb78d768c014dea07c5485eb5819ab42
Shrikant Prabhu <prabhushrikant@gmail.com>
2025-10-16 03:24:23 -0700

    visitor pattern readme.

---
+++ src/main/java/com/shrikant/designpatterns/gof/behavioral/visitor/VisitorApp.java
@@ -0,0 +1,11 @@
+package com.shrikant.designpatterns.gof.behavioral.visitor;
+
+// Visitor Design Pattern
+// Helps in adding a functionality to a group of classes (implementing a common interface) without changing the classes
+// Advantage: Same can be done using Decorator pattern but that would require to create a decorator class for each of the classes
+// Visitor pattern requires to create a visitor interface and concrete visitor class implementing the visitor interface
+
+// Disadvantage: If we need to add a new class to the group of classes, we need to change the visitor interface and all the concrete visitor classes
+public class VisitorApp {
+    
+}
