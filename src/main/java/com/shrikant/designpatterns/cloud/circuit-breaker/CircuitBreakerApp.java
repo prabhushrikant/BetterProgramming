3aa28f0188eea69072f63692f3e6879023505085
Shrikant Prabhu <prabhushrikant@gmail.com>
2025-10-23 01:20:26 -0700

    Add initial implementation of CircuitBreakerApp with references Added graph code.

---
+++ src/main/java/com/shrikant/designpatterns/cloud/circuit-breaker/CircuitBreakerApp.java
@@ -0,0 +1,8 @@
+/**
+ * Reference: https://learn.microsoft.com/en-us/azure/architecture/patterns/circuit-breaker
+ * 
+ * Spring Cloud Circuit Breaker implementation reference: https://www.geeksforgeeks.org/advance-java/spring-boot-circuit-breaker-pattern-with-resilience4j/
+ */
+public class CircuitBreakerApp {
+    
+}
